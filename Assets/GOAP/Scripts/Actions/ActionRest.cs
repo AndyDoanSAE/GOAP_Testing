4985f3f9db6f9363dbd7397c9798aad093b9feaa
Andy <1013651@student.sae.edu.au>
2024-04-17 06:14:56 +1000

    Minor comment adjustment

--- Assets/GOAP/Scripts/Actions/ActionRest.cs
+++ Assets/GOAP/Scripts/Actions/ActionRest.cs
@@ -6,7 +6,7 @@ public class ActionRest : ActionBase
 {
     public override void RunAction()
     {
-        // regenerate stamina
+        // recover stamina
         agent.currentStamina += agent.staminaRegen * Time.deltaTime;
 
         // if the agent's current stamina goes past the maximum, set it to the maximum
